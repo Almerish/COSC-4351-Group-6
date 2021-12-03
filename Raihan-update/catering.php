ad89d84bd2519d2738f5a47050c33ce9295a4f34
raihan1301 <70241381+raihan1301@users.noreply.github.com>
2021-12-02 19:09:22 -0600

    Update catering.php

--- Raihan-update/catering.php
+++ Raihan-update/catering.php
@@ -24,12 +24,12 @@
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
 	      <div class="collapse navbar-collapse" id="navbarSupportedContent1">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"> <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a> </li>
-	          <li class="nav-item active"> <a class="nav-link" href="menu.html">Menu <span class="sr-only">(current)</span></a></li>
+	          <li class="nav-item active"> <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+	          <li class="nav-item active"> <a class="nav-link" href="menu.php">Menu <span class="sr-only">(current)</span></a></li>
 	          <li class="nav-item active"> <a class="nav-link" href="reservation.php">Book A Table <span class="sr-only">(current)</span></a></li>
-	          <li class="nav-item active"> <a class="nav-link" href="catering.html">Private Events <span class="sr-only">(current)</span></a></li>
-				<li class="nav-item active"> <a class="nav-link" href="signup.html">Sign up <span class="sr-only">(current)</span></a></li>
-				<li class="nav-item active"> <a class="nav-link" href="login.html">Log In<span class="sr-only">(current)</span></a></li>
+	          <li class="nav-item active"> <a class="nav-link" href="catering.php">Private Events <span class="sr-only">(current)</span></a></li>
+				<li class="nav-item active"> <a class="nav-link" href="signup.php">Sign up <span class="sr-only">(current)</span></a></li>
+				<li class="nav-item active"> <a class="nav-link" href="login.php">Log In<span class="sr-only">(current)</span></a></li>
             </ul>
 	        <form class="form-inline my-2 my-lg-0">
             </form>
